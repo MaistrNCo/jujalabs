a18e9db2a0f2da77145e3284ed4f97fe005d784a
Mykola Maistrenko <Maistrenko.Mykola@gmail.com>
2017-06-01 16:16:15 +0300

    continue to play with Java.IO streams

--- src/main/java/ua/com/juja/maistrenko/week10/BasicIO.java
+++ src/main/java/ua/com/juja/maistrenko/week10/BasicIO.java
@@ -1,14 +1,18 @@
 package ua.com.juja.maistrenko.week10;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BasicIO {
 
     public static void main(String[] args) {
-        readerWriter();
-        System.out.println();
-        streamReaderWriter();
-        bufferedReaderWriter();
+//        streamReader();
+//        readerWriter();
+//        System.out.println();
+//        streamReaderWriter();
+//        bufferedReaderWriter();
+        URLstreamReaderWriter();
     }
 
     static void streamReaderWriter(){
@@ -25,16 +29,61 @@ public class BasicIO {
         }
 
     }
-    static void readerWriter(){
-        int chunk;
-        try (FileReader inp = new FileReader("input.txt");
-             FileWriter outp = new FileWriter("output.txt")) {
-            while((chunk = inp.read()) != -1){
-                System.out.print((char)chunk);
-                outp.write(chunk);
+
+    static void streamReader(){
+        byte[] chunk = new byte[32];
+
+        try (ByteArrayInputStream inp = new ByteArrayInputStream(new byte[] {})) {
+            while(inp.read(chunk)!= -1 ){
+                System.out.print(chunk.toString());
             }
 
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    static void URLstreamReaderWriter(){
+        String chunk = "";
+        BufferedReader inp = null;
+
+        try (BufferedWriter outp = new BufferedWriter(new FileWriter("output.htm"))) {
+            URL url = new URL("http://google.com.ua");
+            url.openConnection();
+            inp = new BufferedReader(new InputStreamReader(url.openStream()));
+            while(( chunk = inp.readLine()) != null){
+                System.out.println(chunk);
+                outp.write(chunk);
+                outp.newLine();
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inp!=null){
+                try {
+                    inp.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    static void readerWriter(){
+        String chunk;
+        try (BufferedReader inp = new BufferedReader(new FileReader("input.txt"));
+             FileWriter outp = new FileWriter("output.txt")) {
+            while((chunk = inp.readLine()) != null){
+                System.out.print(chunk);
+                outp.write(chunk);
+            }
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -53,4 +102,19 @@ public class BasicIO {
         }
 
     }
+
+     static void dataStreamTest(){
+
+//        try (DataInputStream dis = new DataInputStream(new FileInputStream("input.txt"))){
+//            (char c = dis.readChar())!= null;
+//
+//            while(){
+//                dis.available()
+//            }
+//        } catch (EOFException e) {
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+     }
 }
